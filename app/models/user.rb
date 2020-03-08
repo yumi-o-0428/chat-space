913803ee9e4944b71a4318ad75c436f30532d492
yumi-o-0428 <ochabi0428@gmail.com>
2020-03-08 17:42:42 +0900

    add-groups

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-
-  validates :name, presence: true, uniqueness: true
+  devise  :database_authenticatable, :registerable,
+          :recoverable, :rememberable, :validatable
+  has_many :group_users
+  has_many :groups, through: :group_users
 end
